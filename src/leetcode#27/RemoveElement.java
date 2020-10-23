22debe64091577f19899c3382d55615da8008537
moweiquan <295597613@qq.com>
2020-10-23 23:57:28 +0800

    leetcode-#27removeElement 移除数组中与给出值相等的元素

---
+++ src/leetcode#27/RemoveElement.java
@@ -0,0 +1,42 @@
+/**
+ * Created by mo on 2020/10/23
+ * 题目27: 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素
+ * <p>
+ * 给定 nums = [3,2,2,3], val = 3,
+ * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+ * 你不需要考虑数组中超出新长度后面的元素
+ */
+public class RemoveElement {
+
+    //双指针
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0)
+            return 0;
+
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    //双指针，减少元素复制
+    public int removeElement2(int[] nums, int val) {
+        int i = 0;
+        int len = nums.length;
+        while (i < len) {
+            if (nums[i] == val) {
+                nums[i] = nums[len - 1];
+                len--;
+            } else {
+                i++;
+            }
+        }
+        return len;
+    }
+}
