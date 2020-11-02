281c1cef79f808b45b706b886d24aecea95ec347
moweiquan <295597613@qq.com>
2020-11-02 16:00:21 +0800

    leetcode-#140 wordBreak 单词拆分II

---
+++ src/leetcode#140/wordBreak.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+/**
+ * Created by mo on 2020/11/1
+ * 题目140：给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，
+ * 在字符串中增加空格来构建一个句子，使得句子中所有的单词都在词典中。返回所有这些可能的句子。
+ *
+ * 说明：
+ *分隔时可以重复使用字典中的单词。
+ *你可以假设字典中没有重复的单词
+ *
+ * 输入:
+ * s = "catsanddog"
+ * wordDict = ["cat", "cats", "and", "sand", "dog"]
+ * 输出:
+ * [
+ *   "cats and dog",
+ *   "cat sand dog"
+ * ]
+ *
+ */
+public class wordBreak {
+    //依然使用搜索，不过是记忆化搜索（自顶向下），既能保证性能，还能事先判断拆分的可行性（自底向上的dp则不行）
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Map<Integer, List<List<String>>> map = new HashMap<>();
+        List<List<String>> wordBreaks = backtrack(s, s.length(), new HashSet<String>(wordDict), 0, map);
+        List<String> breakList = new LinkedList<String>();
+        for (List<String> wordBreak : wordBreaks) {
+            breakList.add(String.join(" ", wordBreak));
+        }
+        return breakList;
+    }
+
+    public List<List<String>> backtrack(String s, int length, Set<String> wordSet, int index, Map<Integer, List<List<String>>> map) {
+        if (!map.containsKey(index)) {
+            List<List<String>> wordBreaks = new LinkedList<List<String>>();
+            if (index == length) {
+                wordBreaks.add(new LinkedList<String>());
+            }
+            for (int i = index + 1; i <= length; i++) {
+                String word = s.substring(index, i);
+                if (wordSet.contains(word)) {
+                    List<List<String>> nextWordBreaks = backtrack(s, length, wordSet, i, map);
+                    for (List<String> nextWordBreak : nextWordBreaks) {
+                        LinkedList<String> wordBreak = new LinkedList<String>(nextWordBreak);
+                        wordBreak.offerFirst(word);
+                        wordBreaks.add(wordBreak);
+                    }
+                }
+            }
+            map.put(index, wordBreaks);
+        }
+        return map.get(index);
+    }
+
+}
